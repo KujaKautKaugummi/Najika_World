4b857df659c2e4f82b1d7790423651553e67eb2f
KujaKautKaugummi <0KKK0@proton.me>
2025-11-23 08:34:03 +0100

    Revert "🔧 FIX: API Port von 5000/5173 auf 8000 korrigiert"
    This reverts commit 291d64662715161571421975d037b4f93d15fecc.

--- digivice/static/js/api_client.js
+++ digivice/static/js/api_client.js
@@ -7,7 +7,7 @@
  */
 
 class NajikaAPIClient {
-    constructor(baseURL = 'http://127.0.0.1:8000') {
+    constructor(baseURL = 'http://127.0.0.1:5000') {
         this.baseURL = baseURL;
         this.isOnline = false;
         this.useFallback = true; // Use local fallback if offline
